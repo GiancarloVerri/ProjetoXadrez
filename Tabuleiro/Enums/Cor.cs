3705164b1b004cbba0840599b6cd215c00d53998
Giancarlo <giancarloverri@hotmail.com>
2021-02-20 17:04:46 -0300

    Implementação do tabuleiro iniciado

---
+++ Tabuleiro/Enums/Cor.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Xadrez.Tabuleiro.Enums
+{
+    enum Cor : int
+    {
+        White,
+        Black
+    }
+}
---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,21 @@
+using Projeto_Xadrez.Tabuleiro.Enums;
+
+namespace Projeto_Xadrez.Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
